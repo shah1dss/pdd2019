523f0c614e76157699bb97498a116990d5ba8b6b
DaYmiHn <sawi.denis@yandex.ru>
2019-02-09 16:22:45 +0300

    lc
    Пример вложенного запроса и убранно всё ненужное

--- lc.php
+++ lc.php
@@ -1,52 +1,33 @@
 <?php 
 include("script/link_bd.php");
 
-$strahovka=$_GET['strahovka'];
-
-
-$sql = "SELECT `kod_avto` FROM  `avto` WHERE `strahovka` = '".$strahovka."'";
+$sql = "SELECT * FROM  `drivers` WHERE `kod_avto` =(SELECT `kod_avto` FROM  `avto` WHERE `strahovka` = '".$_GET['strahovka']."')";
 $result = $connection->query($sql);
-$row=$result->fetch();
+$driver=$result->fetch();
 
-$sql = "SELECT * FROM  `drivers` WHERE `kod_avto` = '".$row ['kod_avto']."'";
+$sql = "SELECT * FROM `avto` WHERE `kod_avto`= '".$driver ['kod_avto']."'";
 $result = $connection->query($sql);
-$row=$result->fetch();
+$avto=$result->fetch();
 
+$sql = "SELECT * FROM  `narusheniya` WHERE `kod_avto` = '".$driver ['kod_avto']."'";
+$result = $connection->query($sql);
+$narush=$result->fetch();
 ?>
 
 <link rel="stylesheet" href="style/style.css">
+<haeder><meta charset="UTF-8"></header>
 
 <div class="lcm"> 
 <table id="table-after-search">
-	<haeder><meta charset="UTF-8"></header>
-	<thead>
-		
-	
-    <tr><td>Фамилия:</td> <td><?php echo $row ['surname']; ?></td></tr>
-    <tr><td>Имя:</td>  <td><?php echo $row ['name']; ?></td></tr>
-    <tr><td>Отчество:</td> <td><?php echo $row ['fathername']; ?></td></tr>
-	<?php 
-	$sql = "SELECT * FROM `avto` WHERE `kod_avto`= '".$row ['kod_avto']."'";
-	$result = $connection->query($sql);
-	$row=$result->fetch();
-	 ?>
+    <tr><td>Фамилия:</td> <td><?php echo $driver ['surname']; ?></td></tr>
+    <tr><td>Имя:</td>  <td><?php echo $driver ['name']; ?></td></tr>
+    <tr><td>Отчество:</td> <td><?php echo $driver ['fathername']; ?></td></tr>
 
+    <tr><td>Автомобиль:</td> <td><?php echo $avto ['model']; ?></td></tr>
+    <tr><td>Гос.номер:</td> <td><?php echo $avto ['gos_nomer']; ?></td></tr>
 
-    <tr><td>Автомобиль:</td> <td><?php echo $row ['model']; ?></td></tr>
-    <tr><td>Гос.номер:</td> <td><?php echo $row ['gos_nomer']; ?></td></tr>
-	<?php 
-	$sql = "SELECT * FROM  `narusheniya` WHERE `kod_avto` = '".$row ['kod_avto']."'";
-	$result = $connection->query($sql);
-	$row=$result->fetch();
-	 ?>
-	 <tr><td>Дата нарушения:</td> <td><?php echo $row ['data']; ?></td></tr>
-    <tr><td>Время нарушения:</td> <td><?php echo $row ['time']; ?></td></tr>
-
-
-  	</thead>
-  	<tbody>
-	</tbody>
-
+	<tr><td>Дата нарушения:</td> <td><?php echo $narush ['data']; ?></td></tr>
+    <tr><td>Время нарушения:</td> <td><?php echo $narush ['time']; ?></td></tr>
 </table>
 </div>
 
