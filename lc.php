fa514f7b687db95dd94e3721eb1c2cc6b948ef88
shah1dss <ckeiter@bk.ru>
2019-02-11 23:02:36 +0300

    SUPER
    ВЛОЖЕННЫЕ И ЕЩЕ КАКИЕ-ТО ЗАПРОСЫ СНОВА РАБОТАЮТ!!

--- lc.php
+++ lc.php
@@ -12,6 +12,8 @@ $avto=$result->fetch();
 $sql = "SELECT * FROM  `narusheniya` WHERE `kod_avto` = '".$driver ['kod_avto']."'";
 $result = $connection->query($sql);
 $narush=$result->fetch();
+
+
 ?>
 
 <link rel="stylesheet" href="style/style.css">
@@ -19,59 +21,26 @@ $narush=$result->fetch();
 
 <div class="lcm"> 
 <table id="table-after-search">
-<<<<<<< HEAD
-	<haeder><meta charset="UTF-8"></header>
-	<thead>
-		
-	
-    <tr><td>Фамилия:</td> <td><?php echo $row ['surname']; ?></td></tr>
-    <tr><td>Имя:</td>  <td><?php echo $row ['name']; ?></td></tr>
-    <tr><td>Отчество:</td> <td><?php echo $row ['fathername']; ?></td></tr>
-	<?php 
-	$sql = "SELECT * FROM `avto` WHERE `kod_avto`= '".$row ['kod_avto']."'";
-	$result = $connection->query($sql);
-	$row=$result->fetch();
-	?>
-
-
-    <tr><td>Автомобиль:</td> <td><?php echo $row ['model']; ?></td></tr>
-    <tr><td>Гос.номер:</td> <td><?php echo $row ['gos_nomer']; ?></td></tr>
-
-	<?php 
-	$sql = "SELECT * FROM  `narusheniya` WHERE `kod_avto` = '".$row['kod_avto']."'";
-	$result = $connection->query($sql);
-	$row=$result->fetch();
-	?>
-	 <tr><td>Дата нарушения:</td> <td><?php echo $row ['data']; ?></td></tr>
-    <tr><td>Время нарушения:</td> <td><?php echo $row ['time']; ?></td></tr>
-
-<?php
-$sql = "SELECT `id_narush` FROM  `narusheniya` WHERE `kod_avto` = '".$row ['kod_avto']."'";
-$result = $connection->query($sql);
-$row=$result->fetch();
-
-$sql = "SELECT * FROM  `vid_narush` WHERE `id_narush` = '".$row ['id_narush']."'";
-$result = $connection->query($sql);
-$row=$result->fetch();
-?>
-
- <tr><td>Нарушение:</td> <td><?php echo $row ['name_narush']; ?></td></tr>
-    <tr><td>Цена штрафа:</td> <td><?php echo $row ['price_narush']; ?></td></tr>
-
-  	</thead>
-  	<tbody>
-	</tbody>
-=======
     <tr><td>Фамилия:</td> <td><?php echo $driver ['surname']; ?></td></tr>
     <tr><td>Имя:</td>  <td><?php echo $driver ['name']; ?></td></tr>
     <tr><td>Отчество:</td> <td><?php echo $driver ['fathername']; ?></td></tr>
 
     <tr><td>Автомобиль:</td> <td><?php echo $avto ['model']; ?></td></tr>
     <tr><td>Гос.номер:</td> <td><?php echo $avto ['gos_nomer']; ?></td></tr>
->>>>>>> 523f0c614e76157699bb97498a116990d5ba8b6b
 
-	<tr><td>Дата нарушения:</td> <td><?php echo $narush ['data']; ?></td></tr>
+    <tr><td>Дата нарушения:</td> <td><?php echo $narush ['data']; ?></td></tr>
     <tr><td>Время нарушения:</td> <td><?php echo $narush ['time']; ?></td></tr>
+    <?php 
+    $sql = "SELECT `id_narush` FROM `narusheniya` WHERE `kod_avto` = '".$driver ['kod_avto']."'";
+    $result = $connection->query($sql);
+    $row=$result->fetch();
+
+    $sql = "SELECT * FROM  `vid_narush` WHERE `id_narush` = '".$row ['id_narush']."'";
+    $result = $connection->query($sql);
+    $row=$result->fetch();
+     ?>
+     <tr><td>Нарушение:</td> <td><?php echo $row ['name_narush']; ?></td></tr>
+    <tr><td>Цена штрафа:</td> <td><?php echo $row ['price_narush']; ?></td></tr>
 </table>
 </div>
 
