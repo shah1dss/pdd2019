d305d7bc75cd67a0ff883a4bf216fd19db6f5285
Савин Денис Алексеевич <098-16@std.pkgh.edu>
2019-02-12 15:56:41 +0300

    дизайн админ панели

--- redactorbd.php
+++ redactorbd.php
@@ -2,9 +2,29 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>ОДМИН ПОНЕЛЬ</title>
+	<title>Админ панель</title>
+	<link rel="stylesheet" href="style/style.css">
+	<?php  include("script/link_bd.php");?>
 </head>
 <body>
-	<h3>пока сайт</h3>
+
+	<div class="conteiner1" style="border:2px solid black;padding: 10px;">
+		<table>
+			<tr>
+				<td>kod_avto</td>
+				<td>model</td>
+				<td>gos_nomer</td>
+				<td>strahovka</td>
+			</tr>
+			<tr>
+			<td><input type="text" placeholder="1"></td>
+			<td><input type="text" placeholder="Nissan Teana"></td>
+			<td><input type="text" placeholder="о000оо01"></td>
+			<td><input type="text" placeholder="A23B234123"></td>
+			<td><a href="" class="button9">Добавить</a></td></tr>
+			
+		</table>
+	</div>
+
 </body>
 </html>
